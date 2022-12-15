886d5a9624ffcf114386300c42dbc6351af52808
aravind048 <59740040+aravind048@users.noreply.github.com>
2022-12-16 02:03:43 +0530

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,22 @@
+const users = [
+    { 
+        id:1,
+        name:"John",
+        age: 24,
+    },
+
+    { 
+        id:2,
+        name:"Smith",
+        age: 42,
+    },
+
+    { 
+        id:3,
+        name:"Jane",
+        age: 44,
+    },
+
+];
+
+module.exports = users;
